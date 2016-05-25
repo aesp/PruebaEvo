afd079d8267f7598ab0d810c3a23f679acf4c728
Alex Espino <alexespinog@gmail.com>
2016-05-25 03:09:48 -0500

    Pruebas unitarias

---
+++ TA2-Evolucion/NUnitTestProject/NUnitTest.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TA2_Evolucion;
+using NUnit.Framework;
+
+namespace NUnitTestProject
+{
+    [TestFixture]
+    public class NUnitTest
+    {
+        [Test]
+        public void TestSumar()
+        {
+            TestClass objeto = new TestClass();
+            Assert.AreEqual(7, objeto.Sumar(3, 4));
+        }
+
+        [Test]
+        public void TestMayorA10()
+        {
+            TestClass objeto = new TestClass();
+            Assert.AreEqual("Mayor", objeto.MayorA10(15));
+        }
+
+        [Test]
+        public void TestRestar()
+        {
+            TestClass objeto = new TestClass();
+            Assert.AreNotEqual(6, objeto.Restar(8, 2));
+        }
+
+        [Test]
+        public void TestGetLetra()
+        {
+            TestClass objeto = new TestClass();
+            Assert.AreNotEqual("rro", objeto.SepararCadena("Perro", 2));
+        }
+
+        [Test]
+        public void TestGetDato()
+        {
+            TestClass objeto = new TestClass();
+            Assert.IsNull(objeto.BuscarDato("perro"));
+        }
+
+        [Test]
+        public void TestNumeroEncontrado()
+        {
+            TestClass objeto = new TestClass();
+            Assert.IsTrue(objeto.NumeroEncontrado(8));
+        }
+
+        [Test]
+        public void TestGetLista()
+        {
+            TestClass objeto = new TestClass();
+            List<double> lista = new List<double>();
+            lista.Add(15.5);
+            lista.Add(20.3);
+            lista.Add(30.3);
+
+            CollectionAssert.AreEqual(lista, objeto.GetLista());
+        }
+
+        [Test]
+        public void GetListaNombres()
+        {
+            TestClass objeto = new TestClass();
+            List<string> lista = new List<string>();
+            lista.Add("lapicero");
+            lista.Add("borrador");
+            lista.Add("tinta");
+
+            List<string> actual = objeto.GetListaNombres();
+
+            CollectionAssert.AreEqual(lista, actual);
+        }
+
+        [Test]
+        public void BuscarProductoPorCodigo()
+        {
+            TestClass objeto = new TestClass();
+            Producto esperado = new Producto();
+            esperado.Codigo = 1; esperado.Nombre = "Lapicero"; esperado.Descripcion = "Pilot"; esperado.Precio = 3.5;
+
+            Producto actual = objeto.BuscarProductoPorCodigo(1);
+
+            Assert.AreEqual(esperado.Codigo, actual.Codigo);
+            Assert.AreEqual(esperado.Nombre, actual.Nombre);
+            Assert.AreEqual(esperado.Descripcion, actual.Descripcion);
+            Assert.AreEqual(esperado.Precio, actual.Precio);
+        }
+
+        [Test]
+        public void BuscarProductoPorNombre()
+        {
+            TestClass objeto = new TestClass();
+            Producto esperado = new Producto();
+            esperado.Codigo = 1; esperado.Nombre = "Lapicero"; esperado.Descripcion = "Pilot"; esperado.Precio = 3.5;
+
+            Producto actual = objeto.BuscarProductoPorNombre("Lapicero");
+
+            Assert.AreEqual(esperado.Codigo, actual.Codigo);
+            Assert.AreEqual(esperado.Nombre, actual.Nombre);
+            Assert.AreEqual(esperado.Descripcion, actual.Descripcion);
+            Assert.AreEqual(esperado.Precio, actual.Precio);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestIngresoDato()
+        {
+            TestClass objeto = new TestClass();
+            Producto producto = new Producto();
+            producto.Codigo = 2;
+            producto.Nombre = null;
+            producto.Descripcion = "prueba";
+            try
+            {
+                objeto.IngresoDatos(producto);
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Nombre no existe", ex.Message);
+                throw;
+            }
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestDivision()
+        {
+            try
+            {
+                TestClass objeto = new TestClass();
+                double resultado = objeto.Division(5, 0);
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("No se puede dividir entre cero", ex.Message);
+                throw;
+            }
+        }
+    }
+}
---
+++ TA2-Evolucion/TA2-Evolucion/TestClass.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TA2_Evolucion
+{
+    public class TestClass
+    {
+        //1.- Valores Iguales
+
+        public int Sumar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public string MayorA10(int num)
+        {
+            if (num > 10)
+                return "Mayor";
+            else
+                return "Menor";
+        }
+
+        //2.- Valores Diferentes
+
+        public int Restar(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public string SepararCadena(string palabra, int pos)
+        {
+            return palabra.Substring(pos, palabra.Length-pos);
+        }
+
+        //3.- Valores Nulos
+
+        public string BuscarDato(string palabra)
+        {
+            List<string> lista = new List<string>();
+            lista.Add("perro"); lista.Add("gato");
+
+            foreach (string item in lista)
+            {
+                if (item == palabra)
+                    return item;
+            }
+            return null;
+        }
+
+        //4.- Valores boolean
+
+        public bool NumeroEncontrado(int num)
+        {
+            List<int> lista = new List<int>();
+            lista.Add(8);
+            lista.Add(7);
+            lista.Add(10);
+
+            foreach (int val in lista)
+            {
+                if (val == num)
+                    return true;
+            }
+            return false;
+        }
+
+        //5.- Arreglos
+
+        public List<double> GetLista()
+        {
+            List<double> listaReal = new List<double>();
+            listaReal.Add(15.5);
+            listaReal.Add(20.3);
+            listaReal.Add(30.3);
+
+            return listaReal;
+        }
+
+        public List<string> GetListaNombres()
+        {
+            List<string> lista = new List<string>();
+            lista.Add("lapicero");
+            lista.Add("borrador");
+            lista.Add("tinta");
+            return lista;
+        }
+
+        //6.- Objetos
+
+        public Producto BuscarProductoPorNombre(string nombre)
+        {
+            List<Producto> lista = new List<Producto>();
+            Producto p = new Producto();
+            p.Codigo = 1; p.Nombre = "Lapicero"; p.Descripcion = "Pilot"; p.Precio = 3.5;
+            Producto p1 = new Producto();
+            p1.Codigo = 2; p1.Nombre = "Borrador"; p1.Descripcion = "Staedler"; p.Precio = 2.5;
+            lista.Add(p); lista.Add(p1);
+
+            foreach (Producto prod in lista)
+            {
+                if (prod.Nombre == nombre)
+                    return prod;
+            }
+            return null;
+        }
+
+        public Producto BuscarProductoPorCodigo(int codigo)
+        {
+            List<Producto> lista = new List<Producto>();
+            Producto p = new Producto();
+            p.Codigo = 1; p.Nombre = "Lapicero"; p.Descripcion = "Pilot";p.Precio=3.5;
+            Producto p1 = new Producto();
+            p1.Codigo = 2; p1.Nombre = "Borrador"; p1.Descripcion = "Staedler";p.Precio=2.5;
+            lista.Add(p); lista.Add(p1);
+
+            foreach (Producto prod in lista)
+            {
+                if (prod.Codigo == codigo)
+                    return prod;
+            }
+            return null;
+        }
+
+        //7.- Excepciones
+        public void IngresoDatos(Producto producto)
+        {
+            if (producto.Codigo < 0) throw new Exception("Código debe ser mayor a cero");
+            else if (String.IsNullOrEmpty(producto.Nombre)) throw new ArgumentException("Nombre no existe");
+            else if (String.IsNullOrEmpty(producto.Descripcion)) throw new ArgumentException("Descripción del producto no existe");
+            else if (producto.Precio<0) throw new ArgumentException("Precio debe ser mayor a 0");
+        }
+
+        public double Division(double num1, double num2)
+        {
+            if (num2 == 0) throw new ArgumentException("No se puede dividir entre cero");
+
+            return num1 / num2;
+        }
+
+    }
+}
+
